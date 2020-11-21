3023507da2092f88992f572c2a2aa58bdb6021b6
Cais <62311487+s3nzen@users.noreply.github.com>
2020-11-21 09:32:38 -0500

    adding transit animation

--- No01/script.js
+++ No01/script.js
@@ -1,25 +1,7 @@
 const thumbnails = Array.from(document.querySelectorAll('.thumbnail'));
 
 thumbnails.forEach(pic => {
-    let isEnlarge = false;
     pic.addEventListener('click', event => {
-        let p = event.target;
-        let x = event.target.x;
-        let y = event.target.y;
-        if (! isEnlarge) {
-            p.style.position = 'absolute';
-            p.style.x = 5 + 'px';
-            p.style.y = 5 + 'px';
-            p.style.width = 380 + 'px';
-            p.style.height = 380 + 'px';
-            isEnlarge = true;
-        } else {
-            p.style.position = '';
-            p.style.x = x + 'px';
-            p.style.y = y + 'px';
-            p.style.width = 120 + 'px';
-            p.style.height = 120 + 'px';
-            isEnlarge = false;
-        }
+        event.target.classList.toggle('enlarge');
     })
 })
